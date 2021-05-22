8ad36582f6355993aa2360b14460bb0e836f8eeb
mandar952003 <77447919+mandar952003@users.noreply.github.com>
2021-05-22 16:34:34 +0530

    Add files via upload

---
+++ Tower-Siege-2-Template-main/Tower-Siege-2-Template-main/Block.js
@@ -0,0 +1,28 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      blockimg = loadImage("block.png")
+      this.Visiblity = 255;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed<3){
+        
+      }
+       else{
+      var pos= this.body.position;
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+      push()
+      this.Visiblity = this.Visiblity - 5;
+      tint(255,this.Visiblity);
+       }
+    }
+}
